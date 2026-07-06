7965177e39ad3a17da9b1433e433551d8a2f6811
adamddata-ctrl <lijalem525@gmail.com>
2026-07-07 01:53:05 +0300

    Update MenuController.java

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.repository.MenuRepository;
 @RestController
 @RequestMapping("/api/menu")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
+	@CrossOrigin(origins = {"http://localhost:4200", "https://bliss-front-2x0f.onrender.com"}, allowCredentials = "true")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class MenuController {
     
